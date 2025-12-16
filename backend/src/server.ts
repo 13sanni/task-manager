f0568ac5999e6ee712e2a8695ab0b13305539b09
ss752 <ss7527767@gmail.com>
2025-12-17 02:32:07 +0530

    set up prisma client and mongodb connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,20 @@
 import app from "./app.ts";
 const port = Number(process.env.PORT);
+import { prisma } from "./lib/prisma.ts";
 
+
+async function connectToDatabase() {
+  try {
+    await prisma.$connect();
+    console.log(" Prisma connected to MongoDB");
+  } catch (error) {
+    console.error("Failed to connect Prisma to MongoDB");
+    console.error(error);
+  }
+}
+
+// Connect to the database in the background
+connectToDatabase();
 
 app.listen(port,()=>{
     console.log ("Backend is running")
