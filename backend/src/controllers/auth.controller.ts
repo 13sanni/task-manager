5ab5d828c2d1a33d9b9ca4b22052b2431d72fb67
ss752 <ss7527767@gmail.com>
2025-12-17 18:15:41 +0530

    added register user controller with request validatio

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,1 +1,22 @@
-export{}
+import { Request, Response  } from "express";
+import { z } from "zod";
+import { registerUserSchema } from "../dtos/auth.dto";
+
+export const registerUser = (req: Request,res:Response)=>{
+    const result = registerUserSchema.safeParse(req.body)
+    
+
+  if (!result.success) {
+    const errors = z.treeifyError(result.error);
+
+    return res.status(400).json({
+      message: "Validation failed",
+      errors,
+    });
+  }
+// this is just temp
+  return res.status(201).json({
+    message: "Validation passed",
+    data: result.data,
+  });
+};
